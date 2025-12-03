5cad78c32065db2652f4f531ad999727c8efd640
hazemphoenix <hazemmando408@gmail.com>
2025-12-04 01:19:00 +0200

    Feat: implement AuthFilter to restrict unauthenticated access to all pages except login/registeration

--- src/main/java/com/hazem/filter/AuthFilter.java
+++ src/main/java/com/hazem/filter/AuthFilter.java
@@ -1,4 +1,34 @@
 package com.hazem.filter;
 
-public class AuthFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebFilter("/*")
+public class AuthFilter extends HttpFilter {
+    @Override
+    public void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+        List<String> excludedPaths = List.of("/jsp/login.jsp", "/jsp/register.jsp", "/login", "/register");
+        String requestUri = req.getRequestURI();
+        String path = requestUri.substring(req.getContextPath().length());
+
+        boolean isExcluded = false;
+        for(String p : excludedPaths) {
+            if(p.equals(path)) {
+                isExcluded = true;
+                break;
+            }
+        }
+        if(isExcluded || req.getSession().getAttribute("user") != null) {
+            chain.doFilter(req, res);
+        } else {
+            res.sendRedirect(req.getContextPath() + "/jsp/login.jsp");
+        }
+    }
 }
