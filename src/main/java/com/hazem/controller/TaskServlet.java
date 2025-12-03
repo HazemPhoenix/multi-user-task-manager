377ac1f8b09f8d565cb58ee00435c27620a05a46
hazemphoenix <hazemmando408@gmail.com>
2025-12-03 22:30:08 +0200

    Feat: implement doPost in TaskServlet

--- src/main/java/com/hazem/controller/TaskServlet.java
+++ src/main/java/com/hazem/controller/TaskServlet.java
@@ -1,4 +1,42 @@
 package com.hazem.controller;
 
-public class TaskServlet {
+import com.hazem.config.DatabaseConnectionManager;
+import com.hazem.dao.TaskDAO;
+import com.hazem.dao.TaskDAOImpl;
+import com.hazem.model.User;
+import com.hazem.service.TaskService;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(value = "/tasks/*")
+public class TaskServlet extends HttpServlet {
+    private TaskService taskService;
+
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+        DatabaseConnectionManager dbManager = (DatabaseConnectionManager) servletConfig.getServletContext().getAttribute("DB_MANAGER");
+        TaskDAO taskDAO = new TaskDAOImpl();
+//        this.taskService = new TaskService(dbManager, taskDAO);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        String name = req.getParameter("name");
+        boolean isDone = req.getParameter("isDone") != null;
+        int userId =  ((User) req.getSession().getAttribute("user")).getId();
+        this.taskService.addTask(userId, name, isDone);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
 }
--- src/main/webapp/jsp/add_task.jsp
+++ src/main/webapp/jsp/add_task.jsp
@@ -6,11 +6,22 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page session="false" %>
 <html>
 <head>
-    <title>Title</title>
+    <title>Add a Task</title>
 </head>
 <body>
-
+<form method="post" action="${pageContext.request.contextPath}/tasks/newTask">
+    <div>
+        <label for="name">Task title: </label>
+        <input type="text" name="name" id="name">
+    </div>
+    <div>
+        <label for="isDone">Completed? </label>
+        <input type="checkbox" name="isDone" id="isDone">
+    </div>
+    <button type="submit">Add Task</button>
+</form>
 </body>
 </html>
