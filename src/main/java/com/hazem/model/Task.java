9fffcfd72004f9bdae33e77d12cd8d044279e874
hazemphoenix <hazemmando408@gmail.com>
2025-12-03 19:48:20 +0200

    Feat: implement User and Task models

--- src/main/java/com/hazem/model/Task.java
+++ src/main/java/com/hazem/model/Task.java
@@ -1,4 +1,50 @@
 package com.hazem.model;
 
 public class Task {
+    private int id;
+    private String name;
+    private int userId;
+    private boolean isDone;
+
+    public Task() {
+    }
+
+    public Task(int id, String name, int userId, boolean isDone) {
+        this.id = id;
+        this.name = name;
+        this.userId = userId;
+        this.isDone = isDone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setDone(boolean done) {
+        isDone = done;
+    }
 }
